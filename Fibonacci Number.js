d9df870cf0737bcc821f5ff07d6fed51acf72ea2
danielbontii <bontiidaniel97@gmail.com>
2022-02-23 16:47:02 +0000

    added fibonacci finder first solution

---
+++ Fibonacci Number.js
@@ -0,0 +1,18 @@
+// Fibonacci Finder
+// Create a fibFinder() function that finds the nth number in the Fibonacci sequence. As a reminder, the Fibonacci sequence is a mathematical sequence that begins with 0 and 1, with each following term being the sum of the two previous terms.
+
+// For example, the first two terms of the sequence are represented by fibFinder(0) and fibFinder(1), which return 0 and 1, respectively. fibFinder(6) should return 8.
+
+function fibFinder(n) {
+  // Write your code here
+  //using Binet's Formular
+  const rootFive = Math.sqrt(5);
+  const position = Math.round(
+    (Math.pow((1 + rootFive) / 2, n) - Math.pow((1 - rootFive) / 2, n)) /
+      rootFive
+  );
+
+  return position;
+}
+
+console.log(fibFinder(6));
