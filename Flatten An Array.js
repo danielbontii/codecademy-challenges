6f61c90431d559ec1a8e0ad267fb7aa64997728f
danielbontii <bontiidaniel97@gmail.com>
2022-02-23 15:24:22 +0000

    added flatten array first solution

---
+++ Flatten An Array.js
@@ -0,0 +1,21 @@
+// Flatten an Array
+// Write a function, flattenArray(), that takes in a 2-dimensional array, flattens it into a 1-dimensional array, and returns it. You can assume that you will only be given one or two-dimensional arrays
+
+// For example, flattenArray([1, 2, [3, 4, 5], 6, [7, 8], 9]) should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
+
+function flattenArray(array) {
+  // Write your code here
+  const flattened = [];
+  for (let element of array) {
+    if (typeof element === "object") {
+      for (let elem of element) {
+        flattened.push(elem);
+      }
+    } else {
+      flattened.push(element);
+    }
+  }
+  return flattened;
+}
+
+console.log(flattenArray([1, 2, [3, 4, 5], 6, [7, 8], 9]));
